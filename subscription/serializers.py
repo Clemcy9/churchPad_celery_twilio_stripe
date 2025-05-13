88f869b6bf9cdf6e19d1ec9daa25a5536affe779
clement <clemcy9@gmail.com>
2025-05-14 00:15:14 +0100

    feat: added serializer for subsription model

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import Subscriber
+from .models import Subscriber, Plan
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class SubscriberSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
@@ -10,3 +13,22 @@ class SubscriberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscriber
         fields = ['phone_number', 'is_active']
+
+    def create(self, validated_data):
+        # return super().create(validated_data)
+        name = validated_data.pop('name', None)
+        email = validated_data.pop('email', None)
+        plan_id = validated_data.pop('plan_id', None)
+
+        try:
+            plan = Plan.objects.get(id = plan_id)
+            user = User.objects.create(
+                first_name = name.split(' ')[0],
+                email = email,
+                username = email.split('@')[0]
+            )
+            return Subscriber.objects.create(user =user, plan=plan, **validated_data)
+        except:
+            msg = _('no plain with given id')
+            raise serializers.ValidationError(msg, code='invalid credentials')
+        
--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework import generics, authentication, permissions
+from .serializers import SubscriberSerializer
 # Create your views here.
+
+
